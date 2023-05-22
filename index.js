dd10d62e0eba2f0e8990af198159b4aff3b598ee
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-22 06:35:09 -0700

    module imported properly I think

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // do the needful
-const testLog = require('./lib/classes.js');
+const { testLog, Employee, Manager, Engineer, Intern } = require('./lib/classes.js');
 testLog();
+
+const joe = new Employee( 'bill', 3, "email@fake.com");
+const bob = new Engineer( 'bill', 3, "email2@fake.com", "abloobloo");
+console.log( bob.getRole() );
+console.log( joe.getRole() );
 
